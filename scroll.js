52fd12c8bb781548a4e4b0b43109b3ae8a601b1d
mtlouzada <matheuslouzadaa@gmail.com>
2024-12-28 00:03:35 -0300

    :wastebasket: refactor: remove old scroll function in JavaScript to recreate it in TypeScript (app.tsx)

--- scroll.js
+++
@@ -1,6 +0,0 @@
-const header = document.getElementById("header");
-
-function scrollPage(target) {
-    const element = document.getElementById(target);
-    window.scrollTo(0, element.offsetTop - header.offsetHeight);
-}
